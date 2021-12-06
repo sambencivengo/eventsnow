259a5a0bc1da299be09619d81e40afde3911e2c1
Daniel <daniel.jones424@gmail.com>
2021-12-06 15:42:08 -0500

    Added axios and modified front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, {useEffect, useState} from "react"
 
 function App() {
+
+  const [data, setData] = useState('')
+
+useEffect(() => {
+  axios("localhost:3000/users")
+  .then(resp => {
+    setData(resp.data);
+  })
+  .catch(error => {
+    console.log("error", error);
+  })},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
     </div>
   );
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :locations
   resources :events
   resources :attendances
-  resources :users
+  resources :users, only: [:index]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
