8c8841ec74c4fc58db4a29f56aef083833eba0f4
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-06 14:34:21 -0500

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+40 times do
+  User.create(username: Faker::)
+end
