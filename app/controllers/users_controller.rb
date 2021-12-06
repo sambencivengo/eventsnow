c0edcf6196604f8e144f59568779baf696e33e65
Sam Bencivengo <sambencivengo@gmail.com>
2021-12-06 16:38:31 -0500

    Signup is a go

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create, :index, :destroy]
+
   def index
     users = User.all
     render json: users
+    session[:testing] = "Hello"
   end
+
+  def show
+    user = User.find(params[:id])
+    render json: user    
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {}
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  private
+  
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
